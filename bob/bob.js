c1f9ce07497e5589e145ec5d5bf5e302b578db65
leafyshark <taylor-lindores@hotmail.co.uk>
2018-01-18 23:28:17 +0000

    Submitted bob

--- bob/bob.js
+++ bob/bob.js
@@ -1,26 +1,50 @@
-var Bob = function (input) {
-    this.input = input;
-}
+var Bob = function () {}
 
-Bob.prototype.hey = function () {
-    var whatever = this.input[this.input.length - 1];
+Bob.prototype.hey = function (input) {
+    var inputOne = input.slice(-1);
+    var inputTwo = input[input.length -2];
+    var inputThree = input[input.length -5];
+    var inputter = input.replace(/ /g,'')
 
-    if (whatever === '.' || /[a-z]/.test(whatever)) {
+    console.log(inputter)
+
+    if (/[\s\S]*/.test(inputter) && inputter === '' || /\t/.test(inputter) ) {
+        return 'Fine. Be that way!'
+    }
+
+    if (inputOne === '?' && /[A-Z]/.test(inputThree)) {
+        return 'Calm down, I know what I\'m doing!'
+    }
+
+    if (inputOne === '?') {
+        return 'Sure.';
+
+    } else if (/[\s]/.test(inputOne)) {
+        var inputThree = input = input.replace(/ /g,'');
+
+        if (inputThree.slice(-1) === '?') {
+            return 'Sure.'
+        }
+    }
+
+    if (inputOne === '!' && /[A-Z]/.test(inputTwo) || /[A-Z]/.test(inputOne) || /[0-9]/.test(inputTwo)) {
+        return 'Whoa, chill out!'
+    }
+    
+    if (inputOne === '.' || 
+        /[a-z]/.test(inputOne) ||
+        /[0-9]/.test(inputOne) ||
+        /[a-z]/.test(inputTwo) ||
+        inputOne === '!' && 
+        /[a-z]/.test(inputTwo) ||
+        /[\s]/.test(inputOne)) {
         return 'Whatever.';
     }
 }
 
 module.exports = Bob;
 
-
+// REMEMBER THAT INPUT TOTALLY DEPENDS ON WHAT IS BEING CALLED i.e. is it the constructor function or is it the object?
 // PARAMETERS ARE CALLED WHEN CONSTRUCTING A NEW CLASS
-
-// 'Sure.' (QUESTION MARK)
-
-// 'Whoa, chill out!' (CAPS LOCK)
-
-// 'Calm down, I know what I'm doing!' (QUESTION MARK && CAPS LOCK)
-
-// 'Fine. Be that way!' (EMPTY STRING OR REGEX)
-
-// 'Whatever.' to anything else. (FULL STOP OR NO FULL STOP)
+// PARAMETERS ARE NOT PASSED TO A PROTOTYPAL FUNCTION CALL
+// ALWAYS PASS AN EMPTY STRING TO NEW CLASS DEFINITION
